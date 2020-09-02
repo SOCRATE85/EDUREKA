08588b95ed4a4d4041fc092a0b5d46293607606c
SOCRATE85 <siekapets@gmail.com>
2020-09-01 20:52:42 -0500

    projecttest
    test

--- ConsumeRESTCALL/ConsumeRESTCALL/src/main/java/SurgeprojectRest/Repository/BookMyShowRepository.java
+++
@@ -1,9 +0,0 @@
-package SurgeprojectRest.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import SurgeprojectRest.Model.BookRequest;
-
-public interface BookMyShowRepository extends JpaRepository<BookRequest, Integer>{
-
-}
